02db7697e38b616c5f4a046e5d6173a6120dc37e
VitorVGDev <vgvital@edu.unifil.br>
2025-09-17 21:29:14 -0300

    primeiro commit, com parte inicial do projeto

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -1,7 +1,12 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 
 public class ValidadorTest {
+    @Test
+    void deveValidarCPFValido() {
+        assertTrue(Validador.validarCPF("529.982.247-25"));
+        assertTrue(Validador.validarCPF("52998224725"));
+    }
 }
