a28a927d086bfd2323355087591344c8f152081f
VitorVGDev <vgvital@edu.unifil.br>
2025-09-17 21:33:08 -0300

    segundo commit, rejeitando entradas invalidas

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -8,5 +9,13 @@ public class ValidadorTest {
     void deveValidarCPFValido() {
         assertTrue(Validador.validarCPF("529.982.247-25"));
         assertTrue(Validador.validarCPF("52998224725"));
+    }
+
+    @Test
+    void deveRejeitarEntradasInvalidas() {
+      assertFalse(Validador.validarCPF(null));
+      assertFalse(Validador.validarCPF(""));
+      assertFalse(Validador.validarCPF("529.982.247-2X"));
+      assertFalse(Validador.validarCPF("00000000000"));
     }
 }
